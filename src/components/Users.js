b76c29d042399eec956be150a40b1057c99a1293
KanishkaKodithuwakku <infomail.kbk@gmail.com>
2023-07-23 13:17:27 +0530

    showing initial users and add new user done

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,5 @@
-import React, { useState, useReducer } from "react";
+import React, { useState, useReducer ,useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const ACTIONS = {
   ADD_USER: "add-user",
@@ -8,35 +9,61 @@ const ACTIONS = {
 
 function userReducer(state, action) {
   switch (action.type) {
+    case ACTIONS.ADD_USER:
+
+      console.log(action.payload);
+
+      const newUser = {
+        id: uuidv4(),
+        name: action.payload,
+        age: 90,
+        email: "kanishka@gmail.com",
+      };
+
+    return [...state, newUser];
+
+     
     default:
       return state;
   }
 }
 
 const Users = () => {
+
   const usersArray = [
     {
-      nama: "John Doe",
+      id: uuidv4(),
+      name: "John Doe",
       email: "johndoe@example.com",
       age: "25",
     },
     {
-      nama: "Jane Doe",
+      id: uuidv4(),
+      name: "Jane Doe",
       email: "janedoe@example.com",
       age: "30",
     },
     {
-      nama: "Bob Smith",
+      id: uuidv4(),
+      name: "Bob Smith",
       email: "bobsmith@example.com",
       age: "35",
     },
   ];
-  const [user, setUser] = useState([]);
+  const [user, setUser] = useState('');
   const [state, dispatch] = useReducer(userReducer, usersArray);
+
+  // useEffect(() => {
+  //   dispatch({ type: "" });
+  // }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    //alert(user);
+    dispatch({type:'add-user', payload:user});
   };
+
+
 
   return (
     <>
@@ -52,8 +79,10 @@ const Users = () => {
       </form>
 
       <ul className="list-group">
-        {state.map((user) => ( 
-          <li className="list-group-item">{user.name}</li>
+        {state.map((user) => (
+          <li key={user.id} className="list-group-item">
+            {user.name}
+          </li>
         ))}
       </ul>
     </>
