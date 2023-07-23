3e23b65044aab385d6f0c08a12233dfd671ec5ec
KanishkaKodithuwakku <infomail.kbk@gmail.com>
2023-07-24 00:27:39 +0530

    http client created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import './App.css';
+import { async } from 'q';
+import React, { useState, useEffect} from 'react'
 
-function App() {
+
+const useFetch = url => { 
+
+  const [loading, setloading] = useState(true);
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => { 
+    setloading(true);
+    const fetchApiData = async () => { 
+      
+    }
+
+  },[])
+
+}
+
+const App = () => {
   return (
     <div className="App">
 
