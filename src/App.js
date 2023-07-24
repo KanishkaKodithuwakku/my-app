77fc3bf396ad7a3875f2c32089f8426fbf392841
KanishkaKodithuwakku <infomail.kbk@gmail.com>
2023-07-24 15:13:24 +0530

    fetch data use usereducer instead of usestate

--- src/App.js
+++ src/App.js
@@ -1,30 +1,85 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import httpClient from './utils/httpClient';
 
-const useFetch = url => { 
 
-  const [loading, setloading] = useState(true);
-  const [result, setResult] = useState(null);
-  const [error, setError] = useState(null);
+const initialState = {
+  loading: true,
+  error: null,
+  result:null
+}
 
-  useEffect(() => { 
-    setloading(true);
-    const fetchApiData = async () => { 
+const ACTIONS = {
+  LOADING: 'LOADING',
+  ERROR: 'ERROR',
+  FETCH_DATA: 'FETCH_DATA'
+}
 
+
+function dataFetchReducer(state, action) { 
+
+  switch (action.type) { 
+    case ACTIONS.LOADING:
+      return {
+        loading: true,
+        error: null,
+        result:null
+      }
+    case ACTIONS.ERROR:
+       return {
+         loading: true,
+         error: action.payload.error,
+         result: null,
+       };
+    case ACTIONS.FETCH_DATA:
+      return {
+        result: action.payload.response,
+        loading: false,
+        error:null
+      }
+      
+    default:
+      return state;
+  }
+
+
+
+}
+
+const useFetch = url => {
+  const [state, dispatch] = useReducer(dataFetchReducer, initialState);
+
+  /** 
+   * 
+   * the useState related codes were commented out because you've refactored the component to use useReducer instead of useState.
+   * useState is a hook that lets you add React state to function components, and is great for when you have standalone values.
+   * In contrast, useReducer is usually preferable to useState when you have complex state logic that involves multiple sub-values. It also lets you optimize performance for components that trigger deep updates because you can pass dispatch down instead of callbacks.
+   * In your code, since the state involves multiple sub-values (loading, result, error), using useReducer provides a more optimal and clean solution. That's why the useState related code is no longer needed and has been commented out.
+   */
+
+  // const [loading, setloading] = useState(true);
+  // const [result, setResult] = useState(null);
+  // const [error, setError] = useState(null);
+
+  useEffect(() => {
+    dispatch({ type: ACTIONS.LOADING });
+    //setloading(true);
+    const fetchApiData = async () => {
       try {
         const { data } = await httpClient.get(url);
-        setResult(data);
+        dispatch({ type: ACTIONS.FETCH_DATA, payload: { response: data } });
+        //setResult(data);
       } catch (error) {
-        setError(error.message);
-      } finally { 
-        setloading(false);
-      }
-    }
+        dispatch({ type: ACTIONS.ERROR, payload: { error: error.message } });
+        //setError(error.message);
+      } //finally {
+      //setloading(false);
+      //}
+    };
     fetchApiData();
-  }, [url])
-  
-  return [loading, result, error];
+  }, [url]);
 
+  // return [loading, result, error];
+  return [state.loading, state.result, state.error];
 }
 
 const App = () => {
